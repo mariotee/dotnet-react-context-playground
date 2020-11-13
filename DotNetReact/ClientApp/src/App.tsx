a92a92f2e33ce32685e251856c5ae4558a718aca
mariotee <mariotorresdev@gmail.com>
2020-11-13 13:28:18 -0600

    added more TS support

--- DotNetReact/ClientApp/src/App.tsx
+++ DotNetReact/ClientApp/src/App.tsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router';
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import { Layout } from 'components/Layout';
+import { Home } from 'Pages/Home';
+import { FetchData } from 'Pages/FetchData';
+import { Counter } from 'Pages/Counter';
 
 import './custom.css'
 
