6712974e6c9cb2c9675b2253688bf1de93975d94
mariotee <mariotorresdev@gmail.com>
2020-12-04 08:12:50 -0600

    moved spinner css into module

--- ReactContextPlayground/ClientApp/src/components/Weather/LoadingSpinner/index.tsx
+++ ReactContextPlayground/ClientApp/src/components/Weather/LoadingSpinner/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import "./index.css";
+import styles from "./index.module.css";
 
 export default () => <div className="my-4">
-    <div className="loader"/>
+    <div className={styles.loader}/>
 </div>
